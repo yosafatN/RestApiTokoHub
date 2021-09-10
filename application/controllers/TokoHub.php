6b83be2b883ba806652c7a6d7fd2ef2382d6eadf
yosafatN <yosafatnababan77@gmail.com>
2021-09-10 12:58:22 +0700

    test

--- application/controllers/TokoHub.php
+++ application/controllers/TokoHub.php
@@ -6,6 +6,10 @@ use chriskacerguis\RestServer\RestController;
 
 class TokoHub extends RestController
 {
+    const RESULT_SUCCESS = "Success";
+    const RESULT_EMPTY = "Empty";
+    const RESULT_ERROR = "Error";
+
     public function __construct()
     {
         header('Access-Control-Allow-Origin: *');
@@ -21,7 +25,7 @@ class TokoHub extends RestController
         if ($result) {
             $this->response([
                 'status' => true,
-                'message' => "Success",
+                'message' => self::RESULT_SUCCESS,
                 'data' => $result
             ], RestController::HTTP_OK);
         } else {
@@ -115,6 +119,529 @@ class TokoHub extends RestController
             $this->response([
                 'status' => false,
                 'message' => "Gagal Menghapus Toko"
+            ], RestController::HTTP_OK);
+        }
+    }
+
+    ////////////////// TOKO
+    public function getProduct_get($id_toko)
+    {
+        $result = $this->TokoHubModel->getProduct($id_toko);
+        if ($result) {
+            $this->response([
+                'status' => true,
+                'message' => self::RESULT_SUCCESS,
+                'data' => $result
+            ], RestController::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => true,
+                'message' => self::RESULT_EMPTY
+            ], RestController::HTTP_OK);
+        }
+    }
+
+    public function addProduct_post()
+    {
+        $milliseconds = round(microtime(true) * 1000);
+        $filePath = null;
+        if ($this->post('photo') != '-') {
+            //FILE PATH
+            $filename = $milliseconds . '.' . 'png';
+            $path = "photo_product/";
+            $filePath = $path . $filename;
+
+            //UPLOAD
+            $config = array(
+                'upload_path' => $path,
+                'allowed_types' => "gif|jpg|png|jpeg",
+                'max_size' => '10024',
+                'max_width' => '6000',
+                'max_height' => '6000',
+                'file_name' => $filename
+            );
+            $this->load->library('upload', $config);
+            if (!$this->upload->do_upload('photo')) {
+                $this->response([
+                    'status' => false,
+                    'message' => 'Gagal Menggunggah Foto'
+                ], RestController::HTTP_OK);
+            }
+        }
+        $data = array(
+            'name' => $this->post('name'),
+            'price' => $this->post('price'),
+            'stock' => $this->post('stock'),
+            'unit' => $this->post('unit'),
+            'id_toko' => $this->post('id_toko'),
+            'photo' => $filePath
+        );
+
+        $success = $this->TokoHubModel->addProduct($data);
+        if ($success) {
+            $this->response([
+                'status' => true,
+                'message' => 'Berhasil Tambah Produk'
+            ], RestController::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => 'Gagal Menyimpan Data'
+            ], RestController::HTTP_OK);
+        }
+    }
+
+    public function editProduct_post()
+    {
+        $milliseconds = round(microtime(true) * 1000);
+        $filePath = null;
+        if ($this->post('photo') != '-') {
+            //FILE PATH
+            $filename = $milliseconds . '.' . 'png';
+            $path = "photo_product/";
+            $filePath = $path . $filename;
+
+            //UPLOAD
+            $config = array(
+                'upload_path' => $path,
+                'allowed_types' => "gif|jpg|png|jpeg",
+                'max_size' => '10024',
+                'max_width' => '6000',
+                'max_height' => '6000',
+                'file_name' => $filename
+            );
+            $this->load->library('upload', $config);
+            if (!$this->upload->do_upload('photo')) {
+                $this->response([
+                    'status' => false,
+                    'message' => 'Gagal Menggunggah Foto'
+                ], RestController::HTTP_OK);
+            }
+        }
+        $data = array(
+            'name' => $this->post('name'),
+            'price' => $this->post('price'),
+            'stock' => $this->post('stock'),
+            'unit' => $this->post('unit'),
+            'id' => $this->post('id')
+        );
+        if ($filePath != null) {
+            $data['photo'] = $filePath;
+        }
+
+        $success = $this->TokoHubModel->editProduct($data);
+        if ($success) {
+            $this->response([
+                'status' => true,
+                'message' => 'Berhasil Ubah Produk'
+            ], RestController::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => 'Gagal Menyimpan Data'
+            ], RestController::HTTP_OK);
+        }
+    }
+
+    public function deleteProduct_post()
+    {
+        $id = $this->post('id');
+        $result = $this->TokoHubModel->deleteProduct($id);
+        if ($result) {
+            $this->response([
+                'status' => true,
+                'message' => "Produk Berhasil Dihapus"
+            ], RestController::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => "Gagal Menghapus Produk"
+            ], RestController::HTTP_OK);
+        }
+    }
+
+    // EMPLOYEE
+    public function getEmployee_get($id_toko)
+    {
+        $result = $this->TokoHubModel->getEmployee($id_toko);
+        if ($result) {
+            $this->response([
+                'status' => true,
+                'message' => self::RESULT_SUCCESS,
+                'data' => $result
+            ], RestController::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => true,
+                'message' => self::RESULT_EMPTY
+            ], RestController::HTTP_OK);
+        }
+    }
+
+    public function addEmployee_post()
+    {
+        $milliseconds = round(microtime(true) * 1000);
+        $filePath = null;
+        if ($this->post('photo') != '-') {
+            //FILE PATH
+            $filename = $milliseconds . '.' . 'png';
+            $path = "photo_employee/";
+            $filePath = $path . $filename;
+
+            //UPLOAD
+            $config = array(
+                'upload_path' => $path,
+                'allowed_types' => "gif|jpg|png|jpeg",
+                'max_size' => '10024',
+                'max_width' => '6000',
+                'max_height' => '6000',
+                'file_name' => $filename
+            );
+            $this->load->library('upload', $config);
+            if (!$this->upload->do_upload('photo')) {
+                $this->response([
+                    'status' => false,
+                    'message' => 'Gagal Menggunggah Foto'
+                ], RestController::HTTP_OK);
+            }
+        }
+        $data = array(
+            'name' => $this->post('name'),
+            'phone' => $this->post('phone'),
+            'address' => $this->post('address'),
+            'gender' => $this->post('gender'),
+            'birthDate' => $this->post('birthDate'),
+            'salary' => $this->post('salary'),
+            'id_toko' => $this->post('id_toko'),
+            'photo' => $filePath
+        );
+
+        $success = $this->TokoHubModel->addEmployee($data);
+        if ($success) {
+            $this->response([
+                'status' => true,
+                'message' => 'Berhasil Tambah Pegawai'
+            ], RestController::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => 'Gagal Menyimpan Data'
+            ], RestController::HTTP_OK);
+        }
+    }
+
+    public function editEmployee_post()
+    {
+        $milliseconds = round(microtime(true) * 1000);
+        $filePath = null;
+        if ($this->post('photo') != '-') {
+            //FILE PATH
+            $filename = $milliseconds . '.' . 'png';
+            $path = "photo_employee/";
+            $filePath = $path . $filename;
+
+            //UPLOAD
+            $config = array(
+                'upload_path' => $path,
+                'allowed_types' => "gif|jpg|png|jpeg",
+                'max_size' => '10024',
+                'max_width' => '6000',
+                'max_height' => '6000',
+                'file_name' => $filename
+            );
+            $this->load->library('upload', $config);
+            if (!$this->upload->do_upload('photo')) {
+                $this->response([
+                    'status' => false,
+                    'message' => 'Gagal Menggunggah Foto'
+                ], RestController::HTTP_OK);
+            }
+        }
+        $data = array(
+            'name' => $this->post('name'),
+            'phone' => $this->post('phone'),
+            'address' => $this->post('address'),
+            'gender' => $this->post('gender'),
+            'birthDate' => $this->post('birthDate'),
+            'salary' => $this->post('salary'),
+            'id' => $this->post('id')
+        );
+        if ($filePath != null) {
+            $data['photo'] = $filePath;
+        }
+
+        $success = $this->TokoHubModel->editEmployee($data);
+        if ($success) {
+            $this->response([
+                'status' => true,
+                'message' => 'Berhasil Ubah Pegawai'
+            ], RestController::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => 'Gagal Menyimpan Data'
+            ], RestController::HTTP_OK);
+        }
+    }
+
+    public function deleteEmployee_post()
+    {
+        $id = $this->post('id');
+        $result = $this->TokoHubModel->deleteEmployee($id);
+        if ($result) {
+            $this->response([
+                'status' => true,
+                'message' => "Pegawai Berhasil Dihapus"
+            ], RestController::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => "Gagal Menghapus Pegawai"
+            ], RestController::HTTP_OK);
+        }
+    }
+
+    // SUPPLIER
+    public function getSupplier_get($id_toko)
+    {
+        $result = $this->TokoHubModel->getSupplier($id_toko);
+        if ($result) {
+            $this->response([
+                'status' => true,
+                'message' => self::RESULT_SUCCESS,
+                'data' => $result
+            ], RestController::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => true,
+                'message' => self::RESULT_EMPTY
+            ], RestController::HTTP_OK);
+        }
+    }
+
+    public function addSupplier_post()
+    {
+        $milliseconds = round(microtime(true) * 1000);
+        $filePath = null;
+        if ($this->post('photo') != '-') {
+            //FILE PATH
+            $filename = $milliseconds . '.' . 'png';
+            $path = "photo_supplier/";
+            $filePath = $path . $filename;
+
+            //UPLOAD
+            $config = array(
+                'upload_path' => $path,
+                'allowed_types' => "gif|jpg|png|jpeg",
+                'max_size' => '10024',
+                'max_width' => '6000',
+                'max_height' => '6000',
+                'file_name' => $filename
+            );
+            $this->load->library('upload', $config);
+            if (!$this->upload->do_upload('photo')) {
+                $this->response([
+                    'status' => false,
+                    'message' => 'Gagal Menggunggah Foto'
+                ], RestController::HTTP_OK);
+            }
+        }
+        $data = array(
+            'name' => $this->post('name'),
+            'email' => $this->post('email'),
+            'phone1' => $this->post('phone1'),
+            'phone2' => $this->post('phone2'),
+            'address' => $this->post('address'),
+            'id_toko' => $this->post('id_toko'),
+            'photo' => $filePath
+        );
+
+        $success = $this->TokoHubModel->addSupplier($data);
+        if ($success) {
+            $this->response([
+                'status' => true,
+                'message' => 'Berhasil Tambah Distributor'
+            ], RestController::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => 'Gagal Menyimpan Data'
+            ], RestController::HTTP_OK);
+        }
+    }
+
+    public function editSupplier_post()
+    {
+        $milliseconds = round(microtime(true) * 1000);
+        $filePath = null;
+        if ($this->post('photo') != '-') {
+            //FILE PATH
+            $filename = $milliseconds . '.' . 'png';
+            $path = "photo_supplier/";
+            $filePath = $path . $filename;
+
+            //UPLOAD
+            $config = array(
+                'upload_path' => $path,
+                'allowed_types' => "gif|jpg|png|jpeg",
+                'max_size' => '10024',
+                'max_width' => '6000',
+                'max_height' => '6000',
+                'file_name' => $filename
+            );
+            $this->load->library('upload', $config);
+            if (!$this->upload->do_upload('photo')) {
+                $this->response([
+                    'status' => false,
+                    'message' => 'Gagal Menggunggah Foto'
+                ], RestController::HTTP_OK);
+            }
+        }
+        $data = array(
+            'name' => $this->post('name'),
+            'email' => $this->post('email'),
+            'phone1' => $this->post('phone1'),
+            'phone2' => $this->post('phone2'),
+            'address' => $this->post('address'),
+            'id' => $this->post('id')
+        );
+        if ($filePath != null) {
+            $data['photo'] = $filePath;
+        }
+
+        $success = $this->TokoHubModel->editSupplier($data);
+        if ($success) {
+            $this->response([
+                'status' => true,
+                'message' => 'Berhasil Ubah Distributor'
+            ], RestController::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => 'Gagal Menyimpan Data'
+            ], RestController::HTTP_OK);
+        }
+    }
+
+    public function deleteSupplier_post()
+    {
+        $id = $this->post('id');
+        $result = $this->TokoHubModel->deleteSupplier($id);
+        if ($result) {
+            $this->response([
+                'status' => true,
+                'message' => "Distributor Berhasil Dihapus"
+            ], RestController::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => "Gagal Menghapus Distributor"
+            ], RestController::HTTP_OK);
+        }
+    }
+
+    //ATTENDANCE
+    public function getAttendanceToko_get($id_toko, $startDate, $endDate)
+    {
+        $result = $this->TokoHubModel->getAttendanceToko($id_toko, $startDate, $endDate);
+        if ($result) {
+            $this->response([
+                'status' => true,
+                'message' => self::RESULT_SUCCESS,
+                'data' => $result
+            ], RestController::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => true,
+                'message' => self::RESULT_EMPTY
+            ], RestController::HTTP_OK);
+        }
+    }
+
+    public function getAttendanceEmployee_get($id_employee, $startDate, $endDate)
+    {
+        $result = $this->TokoHubModel->getAttendanceEmployee($id_employee, $startDate, $endDate);
+        if ($result) {
+            $this->response([
+                'status' => true,
+                'message' => self::RESULT_SUCCESS,
+                'data' => $result
+            ], RestController::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => true,
+                'message' => self::RESULT_EMPTY
+            ], RestController::HTTP_OK);
+        }
+    }
+
+    public function getAttendanceHistory_get($id_toko, $date)
+    {
+        $result = $this->TokoHubModel->getAttendanceHistory($id_toko, $date);
+        if ($result) {
+            $this->response([
+                'status' => true,
+                'message' => self::RESULT_SUCCESS,
+                'data' => $result
+            ], RestController::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => true,
+                'message' => self::RESULT_EMPTY
+            ], RestController::HTTP_OK);
+        }
+    }
+
+    public function getPresenceStatus_get($id_toko, $date)
+    {
+        $result = $this->TokoHubModel->getPresenceStatus($id_toko, $date);
+        if ($result) {
+            $this->response([
+                'status' => true,
+                'message' => self::RESULT_SUCCESS,
+                'data' => $result
+            ], RestController::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => true,
+                'message' => self::RESULT_EMPTY
+            ], RestController::HTTP_OK);
+        }
+    }
+
+    public function submitPresence_post()
+    {
+        $id_toko = $this->post('id_toko');
+        $date = $this->post('date');
+        $data = $this->post('data');
+
+        $result = $this->TokoHubModel->submitPresence($id_toko, $date, $data);
+        if ($result) {
+            $this->response([
+                'status' => true,
+                'message' => "Berhasil Menyimpan Data Presensi"
+            ], RestController::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => true,
+                'message' => "Gagal Menyimpan Data Presensi"
+            ], RestController::HTTP_OK);
+        }
+    }
+
+    public function updatePresence_post()
+    {
+        $data = $this->post('data');
+        $result = $this->TokoHubModel->updatePresence($data);
+        if ($result) {
+            $this->response([
+                'status' => true,
+                'message' => "Berhasil Menyimpan Perubahan Data Presensi"
+            ], RestController::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => true,
+                'message' => "Gagal Menyimpan Perubahan Data Presensi"
             ], RestController::HTTP_OK);
         }
     }
--- application/models/TokoHubModel.php
+++ application/models/TokoHubModel.php
@@ -23,4 +23,160 @@ class TokoHubModel extends CI_Model
     {
         return $this->db->delete('Toko', array('id' => $id));
     }
+
+    // PRODUCT
+    public function getProduct($id_toko)
+    {
+        $data = $this->db->order_by('name', 'ASC')->get_where('product', array('id_toko' => $id_toko))->result('array');
+        if ($data == null) {
+            return false;
+        } else {
+            return $data;
+        }
+    }
+
+    public function addProduct($data)
+    {
+        return $this->db->insert('product', $data);
+    }
+
+    public function editProduct($data)
+    {
+        return $this->db->update('product', $data, array('id' => $data['id']));
+    }
+
+    public function deleteProduct($id)
+    {
+        return $this->db->delete('product', array('id' => $id));
+    }
+
+    //EMPLOYEE
+    public function getEmployee($id_toko)
+    {
+        $data = $this->db->order_by('name', 'ASC')->get_where('employee', array('id_toko' => $id_toko))->result('array');
+        if ($data == null) {
+            return false;
+        } else {
+            return $data;
+        }
+    }
+
+    public function addEmployee($data)
+    {
+        return $this->db->insert('employee', $data);
+    }
+
+    public function editEmployee($data)
+    {
+        return $this->db->update('employee', $data, array('id' => $data['id']));
+    }
+
+    public function deleteEmployee($id)
+    {
+        return $this->db->delete('employee', array('id' => $id));
+    }
+
+    //SUPPLIER
+    public function getSupplier($id_toko)
+    {
+        $data = $this->db->order_by('name', 'ASC')->get_where('supplier', array('id_toko' => $id_toko))->result('array');
+        if ($data == null) {
+            return false;
+        } else {
+            return $data;
+        }
+    }
+
+    public function addSupplier($data)
+    {
+        return $this->db->insert('supplier', $data);
+    }
+
+    public function editSupplier($data)
+    {
+        return $this->db->update('supplier', $data, array('id' => $data['id']));
+    }
+
+    public function deleteSupplier($id)
+    {
+        return $this->db->delete('supplier', array('id' => $id));
+    }
+
+    //ATTENDANCE
+    public function getAttendanceToko($id_toko, $startDate, $endDate)
+    {
+        $data = $this->db->query("SELECT id AS id_employee_parent, name, photo, (SELECT COUNT(attendance.id_employee) FROM attendance JOIN presence ON attendance.id_presence = presence.id WHERE attendance.id_employee = id_employee_parent AND attendance.status = 1 AND presence.date BETWEEN '$startDate' AND '$endDate') AS present, (SELECT COUNT(attendance.id_employee) FROM attendance JOIN presence ON attendance.id_presence = presence.id WHERE attendance.id_employee = id_employee_parent AND attendance.status = 0 AND presence.date BETWEEN '$startDate' AND '$endDate') AS absent FROM employee WHERE id_toko = $id_toko ORDER BY name")->result('array');
+        if ($data == null) {
+            return false;
+        } else {
+            return $data;
+        }
+    }
+
+    public function getAttendanceEmployee($id_employee, $startDate, $endDate)
+    {
+        $data = $this->db->query("SELECT attendance.id, presence.date, attendance.status FROM attendance JOIN employee ON attendance.id_employee = employee.id JOIN presence ON attendance.id_presence = presence.id WHERE attendance.id_employee = $id_employee AND presence.date BETWEEN '$startDate' AND '$endDate' ORDER BY presence.date DESC")->result('array');
+        if ($data == null) {
+            return false;
+        } else {
+            return $data;
+        }
+    }
+
+    public function getAttendanceHistory($id_toko, $date)
+    {
+        $data = $this->db->query("SELECT employee.id, employee.name, employee.photo, attendance.status FROM employee JOIN attendance ON employee.id = attendance.id_employee JOIN presence ON presence.id = attendance.id_presence  WHERE presence.date = '$date' AND presence.id_toko = $id_toko ORDER BY employee.name")->result('array');
+
+        if ($data == null) {
+            return false;
+        } else {
+            return $data;
+        }
+    }
+
+    public function getPresenceStatus($id_toko, $date)
+    {
+        $presence = $this->db->query("SELECT id FROM presence WHERE date = '$date' AND id_toko = $id_toko")->result('array');
+        $result = array();
+        if ($presence == null) {
+            $data = $this->db->query("SELECT id, name, photo FROM employee WHERE id_toko = $id_toko ORDER BY name")->result('array');
+            $result = array('status' => 0, 'data' => $data);
+        } else {
+            $id_presence = $presence[0]['id'];
+            $data = $this->db->query("SELECT attendance.id, employee.name, employee.photo, attendance.status FROM attendance JOIN employee ON attendance.id_employee = employee.id JOIN presence ON attendance.id_presence = presence.id WHERE presence.id = $id_presence ORDER BY employee.name")->result('array');
+            $result = array('status' => 1, 'data' => $data);
+        }
+        return $result;
+    }
+
+    public function submitPresence($id_toko, $date, $data)
+    {
+        $this->db->insert('presence', array('id_toko' => $id_toko, 'date' => $date));
+        $id = $this->db->insert_id();
+
+        $temp = array();
+        foreach ($data as $attendance) {
+            $attendance['id_presence'] = $id;
+            array_push($temp, $attendance);
+        }
+
+        return $this->db->insert_batch('attendance', $temp);
+    }
+
+    public function updatePresence($data)
+    {
+        return $this->db->update_batch('attendance', $data, 'id');
+    }
+
+    //STOCK
+    public function getStock($id_toko, $startDate, $endDate)
+    {
+        $data = $this->db->query("SELECT stock.id, supplier.name, supplier.photo, stock.date FROM stock JOIN supplier ON stock.id_supplier = supplier.id WHERE supplier.id_toko = $id_toko AND stock.date BETWEEN '$startDate' AND '$endDate'")->result('array');
+
+        if ($data == null) {
+            return false;
+        } else {
+            return $data;
+        }
+    }
 }
